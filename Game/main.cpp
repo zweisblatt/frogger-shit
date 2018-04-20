1c50f48d61caec9915eb8011c2f42be9838f5055
Oliver <oliverhodge2@gmail.com>
2018-04-20 13:31:31 -0400

    Initial Commit

---
+++ Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Game
+//
+//  Created by Oliver Hodge on 4/20/18.
+//  Copyright © 2018 Oliver Hodge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
